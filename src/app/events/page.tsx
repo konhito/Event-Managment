c9380e79656ab65932d4cba00618d03e49d02a71
konhito <konhito02@gmail.com>
2025-02-09 01:49:06 +0530

    addd a free globe

---
+++ src/app/events/page.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "@/components/Dashboard";
+import Navbar from "@/components/Navbar";
+
+export default function Event() {
+  return (
+    <div className=" min-h-screen bg-slate-950 ">
+      <Navbar />
+      <Dashboard />
+    </div>
+  );
+}
